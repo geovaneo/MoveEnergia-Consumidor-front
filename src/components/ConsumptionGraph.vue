a0d0f5c452ffe849b8c6aea1a73c4cae04b91c23
Gustavo Seemann <seemann24@hotmail.com>
2025-08-13 20:05:47 -0300

    feat: create monthly consumption graphic

---
+++ src/components/ConsumptionGraph.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="w-full">
+    <!-- TITLE -->
+    <div class="w-full pb-[8px] border-b border-[#D2D2D2]">
+      <h2 class="text-[1.375rem] font-bold">Histórico de consumo</h2>
+    </div>
+
+    <!-- GRAPH AND INFO -->
+    <div class="flex justify-between w-full mt-[16px]">
+      <!-- MONTHS -->
+      <div class="flex items-center">
+        <!-- ARROW LEFT -->
+        <div
+          class="h-[50px] w-[50px] hover:bg-orange-50 active:bg-orange-100 transition-all rounded-full flex items-center justify-center mr-[8px]"
+        >
+          <mdicon name="arrow-left" size="40" class="text-primary-orange cursor-pointer" />
+        </div>
+        <div class="flex gap-[16px] items-end">
+          <div
+            v-for="(month, index) in consumptionGraphMonths"
+            :key="index"
+            class="flex flex-col items-center"
+          >
+            <!-- BAR -->
+            <div
+              class="rounded-[5px] min-w-[60px]"
+              :class="`${
+                isSelectedMonth(month.month)
+                  ? 'bg-primary-blue'
+                  : 'bg-primary-blue/20 hover:bg-primary-blue/50 active:bg-primary-blue/80 transition-colors duration-75 cursor-pointer'
+              }`"
+              :style="{ height: `${getBarHeight(month.consumption)}px` }"
+            ></div>
+            <!-- LABEL -->
+            <span class="mt-[8px] text-[0.75rem] font-bold uppercase">
+              {{ month.month.split('/')[0] }} {{ month.month.split('/')[1].slice(-2) }}
+            </span>
+          </div>
+        </div>
+        <!-- ARROW RIGHT -->
+        <div
+          class="h-[50px] w-[50px] hover:bg-orange-50 active:bg-orange-100 transition-all rounded-full flex items-center justify-center ml-[8px]"
+        >
+          <mdicon name="arrow-right" size="40" class="text-primary-orange cursor-pointer" />
+        </div>
+      </div>
+
+      <!-- MONTH INFO -->
+      <div class="flex flex-col items-end text-right">
+        <p class="text-[0.875rem] font-bold text-lighten-blue">Mês de {{ getSelectedMonthText }}</p>
+        <div class="flex items-center gap-[8px]">
+          <mdicon name="lightning-bolt" size="24" class="text-primary-orange" />
+          <p class="text-[1.75rem] font-bold text-primary-blue">
+            {{ consumptionGraphDetails.consumption }} kWh
+          </p>
+        </div>
+        <div class="flex items-center gap-[8px]">
+          <mdicon
+            :name="
+              consumptionGraphDetails.increasedConsumption > 0 ? 'trending-up' : 'trending-down'
+            "
+            size="16"
+            class="text-lighten-blue"
+          />
+          <p class="text-[0.875rem] font-regular text-lighten-blue">
+            {{
+              consumptionGraphDetails.increasedConsumption > 0
+                ? `${consumptionGraphDetails.increasedConsumption}% de aumento no consumo`
+                : `${consumptionGraphDetails.increasedConsumption}% de redução no consumo`
+            }}
+          </p>
+        </div>
+        <div class="mt-[10px] text-[0.875rem] font-semibold">
+          <p class="text-lighten-blue">Período</p>
+          <p class="text-primary-blue">{{ consumptionGraphDetails.period }}</p>
+        </div>
+        <div class="mt-[10px] text-[0.875rem] font-semibold">
+          <p class="text-lighten-blue">Fatura do mês</p>
+          <p class="text-primary-blue">
+            {{
+              Number(consumptionGraphDetails.totalInvoice).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              })
+            }}
+          </p>
+        </div>
+        <div class="mt-[10px] text-[0.875rem] font-semibold">
+          <p class="text-lighten-blue">Economia no mês</p>
+          <p class="text-primary-blue">
+            {{
+              Number(consumptionGraphDetails.monthSavings).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              })
+            }}
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- CONSUMER UNITS -->
+    <div class="mt-[32px]">
+      <h3 class="text-[1.125rem] font-bold text-primary-orange">Unidades Consumidoras</h3>
+    </div>
+    <div
+      class="flex items-center justify-between font-bold text-primary-orange text-[0.875rem] py-[8px] mt-[16px]"
+    >
+      <p class="max-w-[400px] w-full">Unidade</p>
+      <p class="max-w-[150px] w-full text-right">Energia Compensada</p>
+      <p class="max-w-[110px] w-full text-right">Fatura</p>
+      <p class="max-w-[110px] w-full text-right">Desconto</p>
+    </div>
+    <div class="flex flex-col">
+      <div
+        v-for="unit in consumerUnits"
+        :key="unit"
+        class="flex items-center justify-between py-[8px] text-[1rem] font-medium border-b border-[#D2D2D2]"
+      >
+        <p class="max-w-[400px] truncate w-full">{{ unit.name }}</p>
+        <p class="max-w-[150px] w-full text-right">{{ unit.consumption }} kWh</p>
+        <p class="max-w-[110px] w-full text-right">
+          {{
+            Number(unit.totalInvoice).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })
+          }}
+        </p>
+        <p class="max-w-[110px] w-full text-right">
+          {{ Number(unit.savings).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue'
+import { useHomeStore } from '@/stores/home'
+
+const homeStore = useHomeStore()
+
+// -----------------------------------
+// MONTHS LABEL AND BAR SETTINGS
+const consumptionGraphMonths = computed(() => {
+  return [...homeStore.consumptionGraphMonths].reverse()
+})
+const maxBarHeight = 210
+const maxConsumption = computed(() => {
+  return Math.max(...consumptionGraphMonths.value.map((m) => m.consumption))
+})
+
+const getBarHeight = (consumption) => {
+  if (!maxConsumption.value) return 0
+  return (consumption / maxConsumption.value) * maxBarHeight
+}
+
+const isSelectedMonth = (month) => {
+  return month === selectedConsumptionGraphMonth.value
+}
+// -----------------------------------
+// DETAILS SETTINGS
+const consumptionGraphDetails = computed(() => {
+  return homeStore.consumptionGraphDetails
+})
+
+const selectedConsumptionGraphMonth = computed(() => {
+  return homeStore.consumptionGraphDetails.month
+})
+
+const getSelectedMonthText = computed(() => {
+  const monthMap = {
+    JAN: 'Janeiro',
+    FEB: 'Fevereiro',
+    MAR: 'Março',
+    APR: 'Abril',
+    MAY: 'Maio',
+    JUN: 'Junho',
+    JUL: 'Julho',
+    AUG: 'Agosto',
+    SEP: 'Setembro',
+    OCT: 'Outubro',
+    NOV: 'Novembro',
+    DEC: 'Dezembro',
+  }
+  if (!selectedConsumptionGraphMonth.value) return ''
+
+  const monthAbbr = selectedConsumptionGraphMonth.value.split('/')[0]
+
+  return monthMap[monthAbbr] || ''
+})
+
+// -----------------------------------
+// CONSUMER UNITS
+
+const consumerUnits = computed(() => {
+  return homeStore.consumptionGraphDetails.consumerUnits
+})
+
+onMounted(() => {
+  // selectedConsumptionGraphMonth.value = homeStore.consumptionGraphMonths[0].month
+})
+</script>
